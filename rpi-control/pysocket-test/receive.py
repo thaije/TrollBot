c37376b9a3f87d0feb27ac89d9ee931f11846ae9
Tjalling Haije <tjalling_haije@outlook.com>
2018-12-25 13:10:36 +0100

    socket tests

---
+++ rpi-control/pysocket-test/receive.py
@@ -0,0 +1,13 @@
+import socket
+import time
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print ("received message, took:", (time.time() - float(data.decode())) * 1000 , "ms")
